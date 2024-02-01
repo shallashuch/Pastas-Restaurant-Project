8dceb5eca18a996449347307635207b62339da74
shallashuch <massa.ilaria.96@gmail.com>
2024-02-01 18:05:20 +0100

    choice-button done

--- script.js
+++ script.js
@@ -43,28 +43,68 @@ function nextSlide() {
   showSlide(slideIndex);
 }
 
+
 // SHOW BUTTON DELIVERY-TAKEAWAY CONTENT
 const deliveryBtnContainer = document.querySelector('.delivery-button');
 const takeAwayBtnContainer = document.querySelector('.takeaway-button');
-const paperDeliveryContainer = document.querySelector('.paper-left-container');
-const paperTakeAwayContainer = document.querySelector('.paper-right-container');
+const paperContainer = document.querySelector('.paper-container');
+const paperDelivery = document.querySelector('.text-paper-container-delivery');
+const paperTakeAway = document.querySelector('.text-paper-container-takeaway');
+const choiceButton = document.querySelectorAll('.choice-button');
 
-function deliverySlide() {
-  takeAwayBtnContainer.classList.remove('takeaway-transitioned');
-  deliveryBtnContainer.classList.remove('delivery-disappearence');
-  paperTakeAwayContainer.classList.remove('display-container-takeaway');
+const delivery = 'delivery';
+const takeaway = 'takeaway';
 
-  deliveryBtnContainer.classList.toggle('delivery-transitioned');
-  takeAwayBtnContainer.classList.toggle('takeaway-disappearence');
-  paperDeliveryContainer.classList.toggle('display-container');
+function showPaper(action) {
+  if (action === 'delivery') {
+    takeAwayBtnContainer.classList.remove('takeaway-transitioned');
+    deliveryBtnContainer.classList.remove('delivery-disappearence');
+    paperContainer.classList.remove('display-container');
+    paperDelivery.classList.remove('noshow-delivery');
+    deliveryBtnContainer.classList.toggle('delivery-transitioned');
+    takeAwayBtnContainer.classList.toggle('takeaway-disappearence');
+    paperContainer.classList.toggle('display-container');
+    paperTakeAway.classList.toggle('noshow-takeaway');  
+    if (!deliveryBtnContainer.classList.contains('delivery-transitioned')) {
+      takeAwayBtnContainer.classList.remove('takeaway-disappearence');
+      paperContainer.classList.remove('display-container');
+      paperDelivery.classList.remove('noshow-delivery');
+    }
+  } else {
+    deliveryBtnContainer.classList.remove('delivery-transitioned');
+    takeAwayBtnContainer.classList.remove('takeaway-disappearence');
+    paperContainer.classList.remove('display-container');
+    paperTakeAway.classList.remove('noshow-takeaway');
+    takeAwayBtnContainer.classList.toggle('takeaway-transitioned');
+    deliveryBtnContainer.classList.toggle('delivery-disappearence');
+    paperContainer.classList.toggle('display-container');
+    paperDelivery.classList.toggle('noshow-delivery');
+    if (!takeAwayBtnContainer.classList.contains('takeaway-transitioned')) {
+      deliveryBtnContainer.classList.remove('delivery-disappearence');
+      paperContainer.classList.remove('display-container');
+      paperTakeAway.classList.remove('noshow-takeaway');
+    }
+  }
 }
 
-function takeAwaySlide() {
-  deliveryBtnContainer.classList.remove('delivery-transitioned');
-  takeAwayBtnContainer.classList.remove('takeaway-disappearence');
-  paperDeliveryContainer.classList.remove('display-container');
+const closeButton = document.querySelector('.close-paper-button');
 
-  takeAwayBtnContainer.classList.toggle('takeaway-transitioned');
-  deliveryBtnContainer.classList.toggle('delivery-disappearence');
-  paperTakeAwayContainer.classList.toggle('display-container-takeaway');
+function closePaper() {
+  if (!deliveryBtnContainer.classList.contains('delivery-transitioned')) {
+    deliveryBtnContainer.classList.remove('delivery-transitioned');
+    takeAwayBtnContainer.classList.remove('takeaway-transitioned');
+    deliveryBtnContainer.classList.remove('delivery-disappearence');
+    takeAwayBtnContainer.classList.remove('takeaway-disappearence');
+    paperContainer.classList.remove('display-container');
+    paperDelivery.classList.remove('noshow-delivery');
+    paperTakeAway.classList.remove('noshow-takeaway');
+  } else if (!takeAwayBtnContainer.classList.contains('takeaway-transitioned')) {
+    deliveryBtnContainer.classList.remove('delivery-transitioned');
+    takeAwayBtnContainer.classList.remove('takeaway-transitioned');
+    deliveryBtnContainer.classList.remove('delivery-disappearence');
+    takeAwayBtnContainer.classList.remove('takeaway-disappearence');
+    paperContainer.classList.remove('display-container');
+    paperDelivery.classList.remove('noshow-delivery');
+    paperTakeAway.classList.remove('noshow-takeaway');
+  }
 }
