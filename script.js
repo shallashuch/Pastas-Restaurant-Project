b301f81f2606c6cd038ac155722d0c3653799423
shallashuch <massa.ilaria.96@gmail.com>
2024-02-13 10:25:54 +0100

    adjust js

--- script.js
+++ script.js
@@ -76,71 +76,48 @@ function nextSlide() {
   slideIndex++;
   showSlide(slideIndex);
 }
-
+ 
 
 // SHOW BUTTON DELIVERY-TAKEAWAY CONTENT -HOMEPAGE
+const containerBtn = document.getElementById('container-initial-button');
 const deliveryBtnContainer = document.querySelector('.delivery-button');
 const takeAwayBtnContainer = document.querySelector('.takeaway-button');
-const paperContainer = document.querySelector('.paper-container');
-const paperDelivery = document.querySelector('.text-paper-container-delivery');
-const paperTakeAway = document.querySelector('.text-paper-container-takeaway');
+const paperDelivery = document.getElementById('delivery-container');
+const paperTakeAway = document.getElementById('takeaway-container');
 const choiceButton = document.querySelectorAll('.choice-button');
 
 const delivery = 'delivery';
 const takeaway = 'takeaway';
 
 function showPaper(action) {
+
   if (action === 'delivery') {
-    takeAwayBtnContainer.classList.remove('takeaway-transitioned');
-    deliveryBtnContainer.classList.remove('delivery-disappearence');
-    paperContainer.classList.remove('display-container');
-    paperDelivery.classList.remove('noshow-delivery');
-    deliveryBtnContainer.classList.toggle('delivery-transitioned');
-    takeAwayBtnContainer.classList.toggle('takeaway-disappearence');
-    paperContainer.classList.toggle('display-container');
-    paperTakeAway.classList.toggle('noshow-takeaway');  
-    if (!deliveryBtnContainer.classList.contains('delivery-transitioned')) {
-      takeAwayBtnContainer.classList.remove('takeaway-disappearence');
-      paperContainer.classList.remove('display-container');
-      paperDelivery.classList.remove('noshow-delivery');
+    if (paperDelivery.classList.contains('display-container')) {
+      paperDelivery.classList.remove('display-container');
+      containerBtn.classList.remove('initialbtn-nodisplay');
+    } else {
+      paperDelivery.classList.add('display-container');
+      containerBtn.classList.add('initialbtn-nodisplay');
     }
-  } else {
-    deliveryBtnContainer.classList.remove('delivery-transitioned');
-    takeAwayBtnContainer.classList.remove('takeaway-disappearence');
-    paperContainer.classList.remove('display-container');
-    paperTakeAway.classList.remove('noshow-takeaway');
-    takeAwayBtnContainer.classList.toggle('takeaway-transitioned');
-    deliveryBtnContainer.classList.toggle('delivery-disappearence');
-    paperContainer.classList.toggle('display-container');
-    paperDelivery.classList.toggle('noshow-delivery');
-    if (!takeAwayBtnContainer.classList.contains('takeaway-transitioned')) {
-      deliveryBtnContainer.classList.remove('delivery-disappearence');
-      paperContainer.classList.remove('display-container');
-      paperTakeAway.classList.remove('noshow-takeaway');
+    paperTakeAway.classList.remove('display-container');
+  } else if (action === 'takeaway'){
+    if (paperTakeAway.classList.contains('display-container')) {
+      paperTakeAway.classList.remove('display-container');
+      containerBtn.classList.remove('initialbtn-nodisplay');
+    } else {
+      paperTakeAway.classList.add('display-container');
+      containerBtn.classList.add('initialbtn-nodisplay');
     }
+    paperDelivery.classList.remove('display-container');
   }
 }
 
 const closeButton = document.querySelector('.close-paper-button');
 
 function closePaper() {
-  if (!deliveryBtnContainer.classList.contains('delivery-transitioned')) {
-    deliveryBtnContainer.classList.remove('delivery-transitioned');
-    takeAwayBtnContainer.classList.remove('takeaway-transitioned');
-    deliveryBtnContainer.classList.remove('delivery-disappearence');
-    takeAwayBtnContainer.classList.remove('takeaway-disappearence');
-    paperContainer.classList.remove('display-container');
-    paperDelivery.classList.remove('noshow-delivery');
-    paperTakeAway.classList.remove('noshow-takeaway');
-  } else if (!takeAwayBtnContainer.classList.contains('takeaway-transitioned')) {
-    deliveryBtnContainer.classList.remove('delivery-transitioned');
-    takeAwayBtnContainer.classList.remove('takeaway-transitioned');
-    deliveryBtnContainer.classList.remove('delivery-disappearence');
-    takeAwayBtnContainer.classList.remove('takeaway-disappearence');
-    paperContainer.classList.remove('display-container');
-    paperDelivery.classList.remove('noshow-delivery');
-    paperTakeAway.classList.remove('noshow-takeaway');
-  }
+  paperDelivery.classList.remove('display-container');
+  paperTakeAway.classList.remove('display-container');
+  containerBtn.classList.remove('initialbtn-nodisplay');
 }
 
 // WINE AND FOOD MENU -MENU PAGE
